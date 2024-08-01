c82d3b543f5e52f31e2532303d9ece865d4c5279
Miguel Aruelo <miguelenzoaruelo@gmail.com>
2024-08-01 11:57:50 -0400

    fdv

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-import os
-import dotenv
-import random
-import datetime
-import typing
-
 import discord
 from discord.ext import commands
 from discord import app_commands
+
+import os
+import dotenv
+
+import random
+import datetime
+import typing
+from enum import Enum
 
 dotenv.load_dotenv() # NOTE to set the token, create your own .env file, and add the line "token=YOUR_TOKEN_HERE"
 TOKEN = os.getenv('token')
@@ -25,33 +27,45 @@ async def on_ready():
         print(f'Connected to {len(guilds)} guilds!')
         for guild in guilds:
             print(f'  {guild.name} -> ({guild.id})')
+# dice roller
 
-@bot.hybrid_command(name="roll", description="Roll one or multiple dice") # TODO use a universal class for Dice in order to make future commands easier to implement
-async def roll(ctx, die: str, count: int = 1, stat: str = None):
-    if die in ['d100', 'd20', 'd12', 'd10', 'd8', 'd6', 'd4']: # check if the die type is valid
-        pass
-    else:
-        await ctx.send("Invalid die type. Please use d4, d6, d8, d12, d20, or d100.")
-        return
-    sides = int(die[1:]) # grab the number of sides from the die type
-    rolls = [random.randint(1, sides) for _ in range(count)] # roll the die the specified number of times
+class DiceType(Enum):
+    D4 = 4
+    D6 = 6
+    D8 = 8
+    D10 = 10
+    D12 = 12
+    D20 = 20
+    def __str__(self):
+        return f'd{self.value}'
+    def roll(self):
+        return random.randint(1, self.value)
 
-    stat_header = f" for {stat}" if stat != None else "" # REVIEW after implementing character sheets, deprecate this feature
-    embed = discord.Embed(title=f"Rolling {count}d{sides}{stat_header}", description=f"{rolls} -> {sum(rolls)}") # CLEANUP this part is messy af
-    for roll in enumerate(rolls):
-        embed.add_field(name=f"Roll {roll[0] + 1}", value=roll[1], inline=False)
+@bot.hybrid_command(name="roll", description="Roll one or multiple dice")
+@app_commands.describe(
+    count='How many dice to roll.',
+    dice_type='Which dice you want to roll.',
+    modifier='Modifier is applied after all dice have been rolled.'
+)
+async def roll(ctx, count: int, dice_type: DiceType, modifier: int):
+    embed=discord.Embed(
+        title="Dice Roller",
+        description=f'You rolled a {count}{dice_type} **{"+" if modifier >= 0 else "-"}{abs(modifier)}**. Let\'s see how it turned out',
+        color=discord.Color.red()
+        )
+    dicestring = ""
+    runningcount = 0
+    rolls = [dice_type.roll() for _ in range(count)] # roll the die the specified number of times
+    for dicenum in range(count):
+        runningcount += rolls[dicenum]
+        dicestring += f'Dice {dicenum + 1}: {rolls[dicenum]}\n'
+    embed.add_field(name="Dice Outcomes", value=dicestring, inline=False)
+    embed.add_field(name="Dice Total", value=f'Base: {runningcount}\nWith Modifier: {runningcount} **{"+" if modifier >= 0 else "-"}{abs(modifier)}** = {runningcount + modifier}', inline=True)
     embed.timestamp = datetime.datetime.now()
-    embed.set_footer(text=f"ran by {ctx.author.name}", icon_url="https://cdn.discordapp.com/avatars/{ctx.author.id}/{ctx.author.avatar}.png") # FIXME URL doesn't work
-    
-    await ctx.send(embed=embed)
 
-@roll.autocomplete("die") # TODO review this
-async def roll_autocompletion(interaction: discord.Interaction, current: str) -> typing.List[app_commands.Choice[str]]:
-    data = []
-    for die_choice in ['d100', 'd20', 'd12', 'd10', 'd8', 'd6', 'd4']:
-        if current.lower() in die_choice.lower():
-            data.append(app_commands.Choice(name=die_choice, value=die_choice))
-    
-    return data
-    
+    embed.set_footer(text=f"Ran by {ctx.author.name}", icon_url=ctx.author.avatar) # FIXME URL doesn't work
+
+    await ctx.send(embed=embed)
+    # await interaction.response.send_message(f'Okay, you rolled {count}{dice_type}')
+
 bot.run(TOKEN)
